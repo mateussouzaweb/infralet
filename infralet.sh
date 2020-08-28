b6654d3432ea9438c34755c102c1ad58bfcc14c0
Mateus Souza <mateussouzaweb@gmail.com>
2020-08-28 14:21:13 -0300

    Fully updated script to allow subcomands usage

--- infralet.sh
+++ infralet.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-export INFRALET_VERSION="0.0.14"
-export RUN_PATH="$(pwd)"
-export RUN_CMD=( $@ )
+export INFRALET_VERSION="0.0.2"
+export INFRALET_RUN_PATH="$(pwd)"
+
+##
+## PRINT FUNCTIONS
+##
 
 # Printing colored text
 # @param $1 expression
 # @param $2 color
-# @param $3 arrow
-colored_echo() {
+__colored_echo() {
 
     local MESSAGE="$1";
     local COLOR="$2";
-    local ARROW="$3";
 
     if ! [[ $COLOR =~ '^[0-9]$' ]] ; then
        case $(echo $COLOR | tr '[:upper:]' '[:lower:]') in
@@ -30,43 +31,47 @@ colored_echo() {
     if [[ -t 1 ]]; then
         tput bold;
         tput setaf "$COLOR";
-        echo "$ARROW $MESSAGE";
+        echo "$MESSAGE";
         tput sgr0;
     else
-        echo "$ARROW $MESSAGE";
+        echo "$MESSAGE";
     fi
 
 }
 
 # Print info message
 # @param $1 message
-info() {
-    colored_echo "$1" blue "=>"
+__info() {
+    __colored_echo "$1" blue
 }
 
 # Print warning message
 # @param $1 message
-warning() {
-    colored_echo "$1" yellow "=>"
+__warning() {
+    __colored_echo "$1" yellow
 }
 
 # Print success message
 # @param $1 message
-success() {
-    colored_echo "$1" green "=>"
+__success() {
+    __colored_echo "$1" green
 }
 
 # Print error message
 # @param $1 message
-error() {
-    colored_echo "$1" red "=>"
+__error() {
+    __colored_echo "$1" red
 }
+
+##
+## ASK FUNCTIONS
+##
 
 # Ask for a response
 # @param $1 variable name
 # @param $2 default value
 # @param $3 question
-ask() {
+__ask() {
 
     local VARIABLE="$1"
     local DEFAULT="$2"
@@ -90,14 +95,14 @@ ask() {
 # @param $1 variable name
 # @param $2 default value
 # @param $3 question
-ask_yes_no() {
+__ask_yes_no() {
 
     local VARIABLE="$1"
     local DEFAULT="$2"
     local QUESTION="$3"
 
     while true; do
-        ask $VARIABLE $DEFAULT "$QUESTION (Y/N)"
+        __ask $VARIABLE $DEFAULT "$QUESTION (Y/N)"
         case ${!VARIABLE} in
             [Yy]*) export "$VARIABLE=Y"; return 0 ;;
             [Nn]*) export "$VARIABLE=N"; return 1 ;;
@@ -106,22 +111,14 @@ ask_yes_no() {
 
 }
 
-# Ask for sudo password
-ask_sudo_password() {
-
-    if [ ${EUID:-$(id -u)} -eq 0 ]; then
-        success "Sudo credentials OK."
-    else
-        error "Running as not sudo, please use: sudo infralet [command]..."
-        exit 1;
-    fi
-
-}
+##
+## PATH AND FILE FUNCTIONS
+##
 
 # Normalize a file path and return desired info
 # @param $1 file
 # @param $2 type
-normalize() {
+__normalize_path() {
 
     local FILE="$1"
     local TYPE="$2"
@@ -136,40 +133,40 @@ normalize() {
     echo "$REAL"
 }
 
-# manipulate file by symlink, copy or append
+# Manipulate file by symlink, copy or append
 # @param $1 type
 # @param $2 source
 # @param $3 destination
-manipulate_file() {
+__manipulate_file() {
 
     local TYPE="$1"
-    local SOURCE=$(normalize "$2")
-    local DESTINATION=$(normalize "$3")
+    local SOURCE=$(__normalize_path "$2")
+    local DESTINATION=$(__normalize_path "$3")
 
     local TEMPORARY="/tmp/.infralet"
     local OVERWRITTEN=""
 
     # Check source file
     if [ -z "$SOURCE" -o ! -f "$SOURCE" ]; then
-        error "Command $TYPE failed. Source file does not exists: $SOURCE"
+        __error "Command $TYPE failed. Source file does not exists: $SOURCE"
         exit 1;
     fi
 
     # Check if source is symbolic
     if [ -h "$SOURCE" ]; then
-         error "Command $TYPE failed. Source file is symbolic link: $SOURCE"
+        __error "Command $TYPE failed. Source file is symbolic link: $SOURCE"
         exit 1;
     fi
 
     # Check source is equal destination
     if [ "$SOURCE" == "$DESTINATION" ]; then
-        error "Command $TYPE failed. Source file is the same as destination: $SOURCE"
+        __error "Command $TYPE failed. Source file is the same as destination: $SOURCE"
         exit 1;
     fi
 
     # Check destination for append
     if [ "$TYPE" == "append" ] && [ -z "$DESTINATION" -o ! -f "$DESTINATION" ]; then
-        error "Command $TYPE failed. Destination file does not exists: $DESTINATION"
+        __error "Command $TYPE failed. Destination file does not exists: $DESTINATION"
         exit 1;
 
     # Clean destination for copy and symlink
@@ -178,7 +175,7 @@ manipulate_file() {
         if [ -e "$DESTINATION" ] || [ -h "$DESTINATION" ]; then
             OVERWRITTEN="(Overwritten)"
             if ! rm -r "$DESTINATION"; then
-                error "Command $TYPE error. Failed to remove existing file(s) at $DESTINATION."
+                __error "Command $TYPE error. Failed to remove existing file(s) at $DESTINATION."
                 exit 1;
             fi
         fi
@@ -189,32 +186,32 @@ manipulate_file() {
     if [ "$TYPE" == "copy" ]; then
 
         if cp "$SOURCE" "$DESTINATION"; then
-            success "Copied $SOURCE to $DESTINATION. $OVERWRITTEN"
+            __success "Copied $SOURCE to $DESTINATION. $OVERWRITTEN"
         else
-            error "Copy of $SOURCE to $DESTINATION failed."
+            __error "Copy of $SOURCE to $DESTINATION failed."
             exit 1;
         fi
 
-        replace_variables $DESTINATION
+        __replace_variables $DESTINATION
 
     # Append
     elif [ "$TYPE" == "append" ]; then
 
         cat <<< "
 $(cat $SOURCE)" > $TEMPORARY && \
-        replace_variables $TEMPORARY && \
+        __replace_variables $TEMPORARY && \
         cat $TEMPORARY >> $DESTINATION && \
         rm $TEMPORARY
 
-        success "Content of $SOURCE added to $DESTINATION."
+        __success "Content of $SOURCE added to $DESTINATION."
 
     # Symlink
     elif [ "$TYPE" == "symlink" ]; then
 
         if ln -s "$SOURCE" "$DESTINATION"; then
-            success "Symlinked $DESTINATION to $SOURCE. $OVERWRITTEN"
+            __success "Symlinked $DESTINATION to $SOURCE. $OVERWRITTEN"
         else
-            error "Symlinking $DESTINATION to $SOURCE failed."
+            __error "Symlinking $DESTINATION to $SOURCE failed."
             exit 1;
         fi
 
@@ -226,87 +223,43 @@ $(cat $SOURCE)" > $TEMPORARY && \
 # Also replace variables inside a file
 # @param $1 source
 # @param $2 destination
-copy() {
-    manipulate_file "copy" "$1" "$2"
+__copy() {
+    __manipulate_file "copy" "$1" "$2"
 }
 
 # Append content on file
 # Also replace variables inside the file
 # @param $1 source
 # @param $2 destination
-append() {
-    manipulate_file "append" "$1" "$2"
+__append() {
+    __manipulate_file "append" "$1" "$2"
 }
 
 # Make symbolic link
 # @param $1 source
 # @param $2 destination
-symlink() {
-    manipulate_file "symlink" "$1" "$2"
+__symlink() {
+    __manipulate_file "symlink" "$1" "$2"
 }
 
-# Replace match on string
-# @param $1 search
-# @param $2 replace
-# @param $3 string
-str_replace(){
-
-    local SEARCH="$1"
-    local REPLACE="$2"
-    local STRING="$3"
-
-    if [ -z "$STRING" ]; then
-        STRING=$(</dev/stdin)
-    fi
-
-    echo $(echo "$STRING" | awk '{gsub("'$SEARCH'","'$REPLACE'"); print}')
-}
-
-# Retrieve cli parsed param
-# @param $1 param
-# @param $2 default
-# @param $3 flag
-get_param() {
-
-    local PARAM="$1"
-    local RESULT="$2"
-    local FLAG="$3"
-    local LEN=${#RUN_CMD[@]}
-
-    if [ "$PARAM" == "_last_" ]; then
-        RESULT=${RUN_CMD[$LEN-1]}
-    elif [ "$PARAM" == "_first_" ]; then
-        RESULT=${RUN_CMD[0]}
-    else
-        for (( i=0; i<$LEN; i++ )); do
-            if [ "${RUN_CMD[ $i ]}" == "$PARAM" -a "$FLAG" == true ]; then
-                break
-            fi
-            if [ "${RUN_CMD[$i]}" == "$PARAM" ]; then
-                RESULT=${RUN_CMD[ ($i + 1) ]}
-                break
-            fi
-        done
-    fi
-
-    echo $RESULT
-
-}
+##
+## VARIABLES FUNCTIONS
+##
 
 # Recursive load variables from directory
 # @param $1 directory
-load_variables() {
+__load_variables() {
 
     local FILE="variables.env"
-    local DIRECTORY=$(normalize "$1" | str_replace "$FILE" "")
-    local LOCATION=$(echo "$DIRECTORY/$FILE" | str_replace "//" "/")
+    local DIRECTORY=$(__normalize_path "$1" | __str_replace "$FILE" "")
+    local LOCATION=$(echo "$DIRECTORY/$FILE" | __str_replace "//" "/")
 
     while [ ! -f "$LOCATION" ]; do
 
         DIRECTORY=$(dirname "$DIRECTORY")
         LOCATION="$DIRECTORY/$FILE"
 
-        if [ "$DIRECTORY" == "$RUN_PATH" ]; then
+        if [ "$DIRECTORY" == "$INFRALET_RUN_PATH" ]; then
             break;
         elif [ "$DIRECTORY" == "/" ]; then
             break;
@@ -315,9 +268,9 @@ load_variables() {
     done
 
     if [ ! -f "$LOCATION" ]; then
-        warning "No $FILE found. You must create or tell the $FILE file. Skipping..."
+        __warning "No $FILE found. You must create or tell the $FILE file. Skipping..."
     else
-        info "Using the $FILE file located at: $LOCATION"
+        __info "Using the $FILE file located at: $LOCATION"
         if [ -s "$LOCATION" ]; then
             export $(grep -v '^#' $LOCATION | xargs)
         fi
@@ -327,31 +280,31 @@ load_variables() {
 
 # Extract and print variables with their default values on stdout
 # @param $1 command
-extract_variables() {
+__extract_variables() {
 
-    local PARSED=$(echo "$1" | str_replace ".infra" "")
-    local LOCATION=$(normalize "$PARSED" "dir")
-    local COMMAND=$(normalize "$PARSED" "name")
+    local PARSED=$(echo "$1" | __str_replace ".infra" "")
+    local LOCATION=$(__normalize_path "$PARSED" "dir")
+    local COMMAND=$(__normalize_path "$PARSED" "name")
     local FILE="$COMMAND.infra"
 
     if [ ! -f "$LOCATION/$FILE" ]; then
-        error "No $FILE found. You must create the $FILE file inside module folder: $LOCATION/$FILE"
+        __error "No $FILE found. You must create the $FILE file inside module folder: $LOCATION/$FILE"
         exit 1;
     fi
 
-    cat $LOCATION/$FILE | awk '/ask |ask_yes_no / {print $0}' | awk 'BEGIN {FPAT = "([^ ]+)|(\"[^\"]+\")"}{for(i=1;i<=NF;i++){gsub(" "," ",$i)} print $2"="$3}'
+    cat $LOCATION/$FILE | awk '/infralet ask |infralet ask_yes_no / {print $0}' | awk 'BEGIN {FPAT = "([^ ]+)|(\"[^\"]+\")"}{for(i=1;i<=NF;i++){gsub(" "," ",$i)} print $2"="$3}'
 
 }
 
 # Replace env variables on file
-# To avoid unnecessary scaping, only works with ${VAR} sintax
+# To avoid unnecessary escaping, only works with ${VAR} sintax
 # @param $1 file
-replace_variables() {
+__replace_variables() {
 
-    local FILE=$(normalize "$1")
+    local FILE=$(__normalize_path "$1")
 
     if [ ! -f "$FILE" ]; then
-        error "File not found to replace variables: $FILE"
+        __error "File not found to replace variables: $FILE"
         exit 1;
     fi
 
@@ -374,19 +327,90 @@ replace_variables() {
 
 }
 
-# Run module command
-execute_command() {
+##
+## HELPER FUNCTIONS
+##
 
-    local VARIABLES=$(get_param "--e")
-    local PARSED=$(get_param "_last_" | str_replace ".infra" "")
+# Allow execution only in sudo mode
+__only_sudo() {
 
-    if [ -z "$PARSED" ]; then
-        error "You must tell the command to run."
+    if [ ${EUID:-$(id -u)} -eq 0 ]; then
+        __success "Sudo credentials OK."
+    else
+        __error "Running as not sudo, please use: sudo infralet [command]..."
         exit 1;
     fi
 
-    local LOCATION=$(normalize "$PARSED" "dir")
-    local COMMAND=$(normalize "$PARSED" "name")
+}
+
+# Replace match on string
+# @param $1 search
+# @param $2 replace
+# @param $3 string
+__str_replace(){
+
+    local SEARCH="$1"
+    local REPLACE="$2"
+    local STRING="$3"
+
+    if [ -z "$STRING" ]; then
+        STRING=$(</dev/stdin)
+    fi
+
+    echo $(echo "$STRING" | awk '{gsub("'$SEARCH'","'$REPLACE'"); print}')
+}
+
+# Retrieve cli parsed param
+# @param $1 param
+# @param $2 default
+# @param $3 flag
+# @param $4 full cmd...
+__get_param() {
+
+    local PARAM="$1"; shift
+    local RESULT="$2"; shift
+    local FLAG="$3"; shift
+
+    local CMD=( $@ )
+    local LEN=${#CMD[@]}
+
+    if [ "$PARAM" == "_last_" ]; then
+        RESULT=${CMD[$LEN-1]}
+    elif [ "$PARAM" == "_first_" ]; then
+        RESULT=${CMD[0]}
+    else
+        for (( i=0; i<$LEN; i++ )); do
+            if [ "${CMD[ $i ]}" == "$PARAM" -a "$FLAG" == true ]; then
+                break
+            fi
+            if [ "${CMD[$i]}" == "$PARAM" ]; then
+                RESULT=${CMD[ ($i + 1) ]}
+                break
+            fi
+        done
+    fi
+
+    echo $RESULT
+
+}
+
+##
+## RUN FUNCTIONS
+##
+
+# Run module command
+__run() {
+
+    local PARSED=$(echo $1 | __str_replace ".infra" "")
+    local VARIABLES=$(__get_param "--e" "" "" $@)
+
+    if [ -z "$PARSED" ]; then
+        __error "You must tell the command to run."
+        exit 1;
+    fi
+
+    local LOCATION=$(__normalize_path "$PARSED" "dir")
+    local COMMAND=$(__normalize_path "$PARSED" "name")
     local FILE="$COMMAND.infra"
 
     if [ -z "$VARIABLES" ]; then
@@ -394,18 +418,18 @@ execute_command() {
     fi
 
     if [ ! -f "$LOCATION/$FILE" ]; then
-        error "No $FILE found. You must create the $FILE file inside module folder: $LOCATION/$FILE"
+        __error "No $FILE found. You must create the $FILE file inside module folder: $LOCATION/$FILE"
         exit 1;
     fi
 
-    info "Using the $FILE file located at: $LOCATION/$FILE"
+    __info "Using the $FILE file located at: $LOCATION/$FILE"
 
-    load_variables $VARIABLES && \
+    __load_variables $VARIABLES && \
     cd $LOCATION && \
     source $FILE && \
-    cd $RUN_PATH
+    cd $INFRALET_RUN_PATH
 
-    success "Module command completed."
+    __success "Module command completed."
 
 }
 
@@ -413,27 +437,29 @@ execute_command() {
 # @param $1 command
 # @param $2 command
 # @param $3 command...
-extract() {
+__extract() {
 
     argc=$#
     argv=("$@")
 
     for (( j=0; j<argc; j++ )); do
-        extract_variables "${argv[j]}"
+        __extract_variables "${argv[j]}"
     done
 
 }
 
 # Print the version message
-version() {
+__version() {
     echo "Infralet version $INFRALET_VERSION"
 }
 
 # Print the help message
-help() {
+__help() {
 
     cat - >&2 <<EOF
-Whatever you do, infralet it!
+Whatever you do, INFRALET it!
+
+== COMMANDS ==
 
 infralet version
     - See the program version.
@@ -441,20 +467,87 @@ infralet version
 infralet help
     - Print this help message.
 
+infralet run [command.infra] [--e variables.env]
+    - Execute a user defined command file.
+
 infralet extract [command] [command...] [command...]
     - Extract variables created by ask commands.
 
-infralet [--e variables.env] [command]
-    - Execute a user defined command.
+== HELPERS ==
+
+infralet info
+    - Print a info message.
+
+infralet warning
+    - Print a warning message.
+
+infralet success
+    - Print a success message.
+
+infralet error
+    - Print a error message.
+
+infralet ask
+    - Ask for a question.
+
+infralet ask_yes_no
+    - Ask for a question with a yes/no response.
+
+infralet copy
+    - Copy a file to a given location.
+
+infralet append
+    - Append the file contents to a given file.
+
+infralet symlink
+    - Symbolic link a path to another file.
+
+infralet only_sudo
+    - Allow the execution only with the sudo user.
+
+== OTHERS (less used) ==
+
+infralet colored_echo
+    - Print a colored message.
+
+infralet normalize_path
+    - Normalize a given path.
+
+infralet manipulate_file
+    - Manipulate a given file.
+
+infralet load_variables
+    - Load variables from file.
+
+infralet extract_variables
+    - Extract variables from file.
+
+infralet replace_variables
+    - Replace current enviroment variables on the given file with the real value.
+
+infralet str_replace
+    - Replace matches on the give string.
+
+infralet get_param
+    - Retrieve CLI param.
 EOF
 
 }
 
-if [[ $1 =~ ^(version|help|extract)$ ]]; then
-    "$@"
-elif [[ ! -z $1 ]]; then
-    execute_command
-else
-    echo "Invalid infralet subcommand: $1" >&2
-    exit 1
-fi
+# Launcher function
+infralet() {
+
+    if type "__$1" >/dev/null 2>&1; then
+        subcommand=$1; shift
+        "__$subcommand" "$@"
+    elif [[ ! -z $1 ]]; then
+        __error "Invalid infralet subcommand: $1." >&2
+        exit 1
+    else
+        __error "Invalid infralet subcommand. The command is missing." >&2
+        exit 1
+    fi
+
+}
+
+infralet "$@"
